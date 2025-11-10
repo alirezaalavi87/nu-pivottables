4b7593139b3a987d98d6841a1d2c413aff8979c6
Alireza Alavi <alavialireza@protonmail.com>
2025-11-10 16:13:21 +0330

    feat(i18n): add Farsi(Persian) localization

---
+++ dist/pivot.fa.js
@@ -0,0 +1,81 @@
+(function() {
+  var callWithJQuery;
+
+  callWithJQuery = function(pivotModule) {
+    if (typeof exports === "object" && typeof module === "object") { // CommonJS
+      return pivotModule(require("jquery"));
+    } else if (typeof define === "function" && define.amd) { // AMD
+      return define(["jquery"], pivotModule);
+    } else {
+      // Plain browser env
+      return pivotModule(jQuery);
+    }
+  };
+
+  callWithJQuery(function($) {
+    var frFmt, frFmtInt, frFmtPct, nf, tpl;
+    nf = $.pivotUtilities.numberFormat;
+    tpl = $.pivotUtilities.aggregatorTemplates;
+    frFmt = nf({
+      thousandsSep: " ",
+      decimalSep: "."
+    });
+    frFmtInt = nf({
+      digitsAfterDecimal: 0,
+      thousandsSep: " ",
+      decimalSep: "."
+    });
+    frFmtPct = nf({
+      digitsAfterDecimal: 1,
+      scaler: 100,
+      suffix: "٪",
+      thousandsSep: " ",
+      decimalSep: "."
+    });
+    return $.pivotUtilities.locales.fa = {
+      localeStrings: {
+        renderError: "در رندر کردن نتیجه ی جدول خطایی رخ داد.",
+        computeError: "در محاسبه ی نتیجه ی جدول خطایی رخ داد.",
+        uiRenderError: "در رندر کردن رابط کاربری جدول خطایی رخ داد.",
+        selectAll: "انتخاب همه",
+        selectNone: "انتخاب هیچکدام",
+        tooMany: "(مقدار لیست بیش از اندازه بزرگ است)",
+        filterResults: "صافی مقادیر",
+        apply: "اعمال تغییرات",
+        cancel: "انصراف",
+        totals: "مجموع",
+        vs: "در مقابل",
+        by: "بر حسب"
+      },
+      aggregators: {
+        "تعداد": tpl.count(frFmtInt),
+        "تعداد منحصر به فرد": tpl.countUnique(frFmtInt),
+        "لیست مقادیر منحصر به فرد": tpl.listUnique(", "),
+        "جمع": tpl.sum(frFmt),
+        "جمع عددی": tpl.sum(frFmtInt),
+        "میانگین": tpl.average(frFmt),
+        "کمینه": tpl.min(frFmt),
+        "بیشینه": tpl.max(frFmt),
+        "مجموع جمع": tpl.sumOverSum(frFmt),
+        "حد بالای ۸۰٪": tpl.sumOverSumBound80(true, frFmt),
+        "حد پایین ۸۰٪": tpl.sumOverSumBound80(false, frFmt),
+        "مجموع به نسبت کل": tpl.fractionOf(tpl.sum(), "total", frFmtPct),
+        "مجموع به نسبت سطر ها": tpl.fractionOf(tpl.sum(), "row", frFmtPct),
+        "مجموع به نسبت ستون ها": tpl.fractionOf(tpl.sum(), "col", frFmtPct),
+        "تعداد به عنوان کسری از کل": tpl.fractionOf(tpl.count(), "total", frFmtPct),
+        "تعداد به عنوان کسری از سطرها": tpl.fractionOf(tpl.count(), "row", frFmtPct),
+        "تعداد به عنوان کسری از ستون ها": tpl.fractionOf(tpl.count(), "col", frFmtPct)
+      },
+      renderers: {
+        "جدول": $.pivotUtilities.renderers["Table"],
+        "جدولِ نمودار میله ای": $.pivotUtilities.renderers["Table Barchart"],
+        "نقشه گرمایشی": $.pivotUtilities.renderers["Heatmap"],
+        "نقشه گرمایشی سطری": $.pivotUtilities.renderers["Row Heatmap"],
+        "نقشه گرمایشی ستونی": $.pivotUtilities.renderers["Col Heatmap"]
+      }
+    };
+  });
+
+}).call(this);
+
+//# sourceMappingURL=pivot.fa.js.map
